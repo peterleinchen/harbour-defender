0d26ec4f057e6556bd64af24edcb1d1cec9de17f
Carmen F. B. <6286776+carmenfdezb@users.noreply.github.com>
2026-04-21 12:31:52 +0200

    Updated Spanish translation (#11)

--- translations/harbour-defender-es.ts
+++ translations/harbour-defender-es.ts
@@ -57,7 +57,7 @@
         <location filename="../qml/pages/CookiesPage.qml" line="83"/>
         <location filename="../qml/pages/CookiesPage.qml" line="162"/>
         <source>Deleting</source>
-        <translation type="unfinished"></translation>
+        <translation>Borrando</translation>
     </message>
     <message>
         <location filename="../qml/pages/CookiesPage.qml" line="56"/>
@@ -67,27 +67,27 @@
     <message>
         <location filename="../qml/pages/CookiesPage.qml" line="111"/>
         <source>Remove</source>
-        <translation>Eliminar</translation>
+        <translation>Quitar</translation>
     </message>
     <message>
         <location filename="../qml/pages/CookiesPage.qml" line="115"/>
         <source>Remove from Whitelist</source>
-        <translation>Eliminar de la lista blanca</translation>
+        <translation>Quitar de la lista blanca</translation>
     </message>
     <message>
         <location filename="../qml/pages/CookiesPage.qml" line="115"/>
         <source>Add to Whitelist</source>
-        <translation>Anadir a la lista blanca</translation>
+        <translation>Añadir a la lista blanca</translation>
     </message>
     <message>
         <location filename="../qml/pages/CookiesPage.qml" line="134"/>
         <source>Remove from Blacklist</source>
-        <translation>Eliminar de la lista negra</translation>
+        <translation>Quitar de la lista negra</translation>
     </message>
     <message>
         <location filename="../qml/pages/CookiesPage.qml" line="134"/>
         <source>Add to Blacklist</source>
-        <translation>Anadir a la lista negra</translation>
+        <translation>Añadir a la lista negra</translation>
     </message>
 </context>
 <context>
@@ -100,7 +100,7 @@
     <message>
         <location filename="../qml/pages/DocsPage.qml" line="35"/>
         <source>The installation and use of this application is solely in the responsibility of the user. The developers/authors are not responsible for the content of ad blocking sources available from the application, therefore you need to be careful when enabling them.</source>
-        <translation>La instalacion y uso de esta aplicacion solo es responsabilidad del usuario. Los desarrolladores/autores no son responsables del contenido de las fuentes de bloqueo de anuncios disponibles en la aplicacion, por tanto, debes tener cuidado cuando los habilites.</translation>
+        <translation>La instalación y uso de esta aplicación sólo es responsabilidad del usuario. Los desarrolladores/autores no son responsables del contenido de las fuentes de bloqueo de anuncios disponibles en la aplicación, por tanto, debes tener cuidado cuando los habilites.</translation>
     </message>
     <message>
         <location filename="../qml/pages/DocsPage.qml" line="41"/>
@@ -110,51 +110,51 @@
     <message>
         <location filename="../qml/pages/DocsPage.qml" line="44"/>
         <source>How to add custom lists?</source>
-        <translation>Como anadir listas personalizadas?</translation>
+        <translation>¿Cómo añadir listas personalizadas?</translation>
     </message>
     <message>
         <source>You can add custom lists by editing the file /etc/%1.conf as root (either using the command line or an appropriate editor). See other sections in the config file for inspiration. In the square brackets [] should be a unique id.</source>
-        <translation type="vanished">Puedes anadir listas personalizadas al hosts editando el fichero /etc/%1.conf como root (ya sea usando la linea de comandos o un editor apropiado). Consulta otras secciones en el archivo de configuracion para la inspiracion. Entre corchetes [] deberia haber un unico id.</translation>
+        <translation type="vanished">Puedes añadir listas personalizadas al hosts editando el fichero /etc/%1.conf como root (ya sea usando la línea de comandos o un editor apropiado). Consulta otras secciones en el archivo de configuración para la inspiración. Entre corchetes [] debería haber un único id.</translation>
     </message>
     <message>
         <location filename="../qml/pages/DocsPage.qml" line="19"/>
         <source>Version</source>
-        <translation type="unfinished"></translation>
+        <translation>Versión</translation>
     </message>
     <message>
         <location filename="../qml/pages/DocsPage.qml" line="49"/>
         <source>You can add custom lists by editing the file /etc/defender.conf as root (either using the command line or an appropriate editor). See other sections in the config file for inspiration. In the square brackets [] should be a unique id.</source>
-        <translation type="unfinished"></translation>
+        <translation>Puedes añadir listas personalizadas editando el archivo /etc/defender.conf como root (ya sea usando la línea de comandos o un editor apropiado). Consulta otras secciones en el archivo de configuración para la inspiración. Entre corchetes [] debería haber un único id.</translation>
     </message>
     <message>
         <location filename="../qml/pages/DocsPage.qml" line="55"/>
         <source>Why can&apos;t I add new sources from the app?</source>
-        <translation>Por que no puedo anadir nuevas fuentes desde la aplicacion?</translation>
+        <translation>¿Por qué no puedo añadir nuevas fuentes desde la aplicación?</translation>
     </message>
     <message>
         <location filename="../qml/pages/DocsPage.qml" line="60"/>
         <source>It would be a security threat to allow adding new sources as a normal user (with the current SailfishOS security situation), as any app would be able to add new sources and potentially compromise your device. Even with the default sources, you still make the leap of faith to trust a remote source. If you want to have a source added/removed to/from the defaults, contact the app developer and see if it can be available in the next version.</source>
-        <translation>Seria una amenaza a la seguridad permitir a un usuario normal anadir nuevas fuentes (con la situacion actual de seguridad de SailfishOS), ya que cualquier aplicacion podria anadir nuevas fuentes y poner en peligro tu dispositivo. Aun con las fuentes por defecto, estarias confiando en una fuente remota. Si quieres anadir/eliminar una fuente a/de las que hay por defecto, contacta con el desarrollador de la aplicacion para ver si se puede incluir en la siguiente version.</translation>
+        <translation>Sería una amenaza a la seguridad permitir a un usuario normal añadir nuevas fuentes (con la situación actual de seguridad de SailfishOS), ya que cualquier aplicación podría añadir nuevas fuentes y poner en peligro tu dispositivo. Aún con las fuentes por defecto, estarías confiando en una fuente remota. Si quieres añadir/eliminar una fuente a/de las que hay por defecto, contacta con el desarrollador de la aplicación para ver si se puede incluir en la siguiente versión.</translation>
     </message>
     <message>
         <location filename="../qml/pages/DocsPage.qml" line="66"/>
         <source>How to add custom entries?</source>
-        <translation>Como anadir entradas personalizadas?</translation>
+        <translation>¿Cómo añadir entradas personalizadas?</translation>
     </message>
     <message>
         <location filename="../qml/pages/DocsPage.qml" line="71"/>
         <source>You can add your custom hosts entries by editing the file /etc/hosts.editable and treating it as a generic hosts file. Don&apos;t forget to choose &apos;Update Now&apos; in the app to see an immediate effect.</source>
-        <translation>Puedes anadir tus entradas personalizadas al hosts editando el fichero /etc/hosts.editable y tratandolo como un fichero hosts generico. No olvides seleccionar &apos;Actualizar ahora&apos; en la aplicacion para ver el efecto inmediato.</translation>
+        <translation>Puedes añadir tus entradas personalizadas al hosts editando el fichero /etc/hosts.editable y tratándolo como un fichero hosts genérico. No olvides seleccionar &apos;Actualizar ahora&apos; en la aplicación para ver el efecto inmediato.</translation>
     </message>
     <message>
         <location filename="../qml/pages/DocsPage.qml" line="77"/>
         <source>Why can&apos;t I add new entries from the app?</source>
-        <translation>Por que no puedo anadir nuevas entradas desde la aplicacion?</translation>
+        <translation>¿Por qué no puedo añadir nuevas entradas desde la aplicación?</translation>
     </message>
     <message>
         <location filename="../qml/pages/DocsPage.qml" line="82"/>
         <source>Again, it would be a security threat (see the answer above), therefore one needs to be root to add/modify hosts file entries.</source>
-        <translation>De nuevo, seria una amenaza a la seguridad (ver respuesta de mas arriba), por tanto, necesitas ser root para anadir/modificar entradas en el fichero hosts.</translation>
+        <translation>De nuevo, sería una amenaza a la seguridad (ver respuesta de más arriba), por tanto, necesitas ser root para añadir/modificar entradas en el fichero hosts.</translation>
     </message>
     <message>
         <location filename="../qml/pages/DocsPage.qml" line="88"/>
@@ -164,7 +164,11 @@
     <message>
         <location filename="../qml/pages/DocsPage.qml" line="93"/>
         <source>The cookie manager works by editing the -/.local/share/org.sailfishos/browser/.mozilla/ (for SFOS up to 3.4: -/.mozilla/mozembed/) cookies.sqlite database. In order to access/work with cookies the browser needs to be closed before opening the cookies section, else you might see an empty window. All changes need the browser to be closed/restarted in order to take effect.</source>
-        <translation type="unfinished"> El gestor de cookies funciona editando la base de datos sqlite /home/nemo/.mozilla/mozembed/cookies.sqlite. Para que todos los cambios tengan efecto es necesario reiniciar el navegador.</translation>
+        <translation>El gestor de cookies funciona editando la base de datos sqlite ubicada en ~/.local/share/org.sailfishos/browser/.mozilla/ (hasta la versión 3.4 de SFOS: ~/.mozilla/mozembed/) . Para poder acceder/trabajar con las cookies, es necesario cerrar el navegador antes de abrir la sección de cookies; de lo contrario, podrías ver una ventana vacía. Para que todos los cambios surtan efecto es necesario cerrar/reiniciar el navegador.</translation>
+    </message>
+    <message>
+        <source>The cookie manager works by editing the /home/nemo/.mozilla/mozembed/cookies.sqlite database. All changes need the browser to be restarted in order to take effect.</source>
+        <translation type="vanished">El gestor de cookies funciona editando la base de datos sqlite /home/nemo/.mozilla/mozembed/cookies.sqlite. Para que todos los cambios tengan efecto es necesario reiniciar el navegador.</translation>
     </message>
     <message>
         <location filename="../qml/pages/DocsPage.qml" line="99"/>
@@ -174,7 +178,7 @@
     <message>
         <location filename="../qml/pages/DocsPage.qml" line="104"/>
         <source>Cookie locking works by making the cookie database read only, therefore its contents stay same between restarting the browser. The effect of this is cookies not being persistent over browser restarts.</source>
-        <translation>El bloqueo de cookies funciona haciendo que la base de datos de cookies sea solo de lectura, por tanto su contenido es el mismo al reiniciar el navegador. El efecto de esto es que las cookies no son persistentes en los reinicios del navegador.</translation>
+        <translation>El bloqueo de cookies funciona haciendo que la base de datos de cookies sea sólo de lectura, por tanto su contenido es el mismo al reiniciar el navegador. El efecto de esto es que las cookies no son persistentes en los reinicios del navegador.</translation>
     </message>
 </context>
 <context>
@@ -193,7 +197,7 @@
         <location filename="../qml/pages/SettingsPage.qml" line="25"/>
         <location filename="../qml/pages/SettingsPage.qml" line="36"/>
         <source>Clearing</source>
-        <translation type="unfinished"></translation>
+        <translation>Limpiando</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="33"/>
@@ -203,12 +207,12 @@
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="44"/>
         <source>WLAN only</source>
-        <translation>Solo WLAN</translation>
+        <translation>Sólo WLAN</translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="45"/>
         <source>Downloads adblock lists only if connected to WLAN (tested only on Jolla phones)</source>
-        <translation>Descarga listas de bloqueo de anuncios solo si esta conectado con WLAN (solo se ha probado en telefonos Jolla)</translation>
+        <translation>Descarga listas de bloqueo de anuncios sólo si está conectado con WLAN (sólo se ha probado en teléfonos Jolla)</translation>
     </message>
 </context>
 <context>
@@ -221,7 +225,7 @@
     <message>
         <location filename="../qml/pages/SourceDetailPage.qml" line="19"/>
         <source>More information</source>
-        <translation>Mas informacion</translation>
+        <translation>Más información</translation>
     </message>
     <message>
         <location filename="../qml/pages/SourceDetailPage.qml" line="43"/>
@@ -255,44 +259,52 @@
 <context>
     <name>SourcesPage</name>
     <message>
+        <source>Disable</source>
+        <translation type="vanished">Desactivar</translation>
+    </message>
+    <message>
+        <source>Preparing</source>
+        <translation type="vanished">Preparando</translation>
+    </message>
+    <message>
         <location filename="../qml/pages/SourcesPage.qml" line="13"/>
         <source>Disable all</source>
-        <translation type="unfinished">Desactivar todo</translation>
+        <translation>Desactivar todo</translation>
     </message>
     <message>
         <location filename="../qml/pages/SourcesPage.qml" line="15"/>
         <source>Preparing disable</source>
-        <translation type="unfinished"></translation>
+        <translation>Preparando desactivación</translation>
     </message>
     <message>
         <location filename="../qml/pages/SourcesPage.qml" line="21"/>
         <source>Cancel/clear update loop</source>
-        <translation type="unfinished"></translation>
+        <translation>Cancelar/limpiar bucle de actualización</translation>
     </message>
     <message>
         <location filename="../qml/pages/SourcesPage.qml" line="23"/>
         <source>Preparing cancel/clear</source>
-        <translation type="unfinished"></translation>
+        <translation>Preparando cancelación/limpieza</translation>
     </message>
     <message>
         <location filename="../qml/pages/SourcesPage.qml" line="29"/>
         <source>Show error log (just in case ;)</source>
-        <translation type="unfinished"></translation>
+        <translation>Mostrar registro de errores (por si acaso ;)</translation>
     </message>
     <message>
         <location filename="../qml/pages/SourcesPage.qml" line="31"/>
         <source>Pulling up error.log (only if exists)</source>
-        <translation type="unfinished"></translation>
+        <translation>Cargando error.log (sólo si existe)</translation>
     </message>
     <message>
         <location filename="../qml/pages/SourcesPage.qml" line="37"/>
         <source>Restart Android/App Support</source>
-        <translation type="unfinished"></translation>
+        <translation>Reinciar Android/AppSupport</translation>
     </message>
     <message>
         <location filename="../qml/pages/SourcesPage.qml" line="39"/>
         <source>Preparing Android restart</source>
-        <translation type="unfinished"></translation>
+        <translation>Preparando reinicio de Android</translation>
     </message>
     <message>
         <location filename="../qml/pages/SourcesPage.qml" line="45"/>
@@ -302,7 +314,7 @@
     <message>
         <location filename="../qml/pages/SourcesPage.qml" line="47"/>
         <source>Preparing update</source>
-        <translation type="unfinished"></translation>
+        <translation>Preparando actualización</translation>
     </message>
     <message>
         <location filename="../qml/pages/SourcesPage.qml" line="56"/>
@@ -317,7 +329,7 @@
     <message>
         <location filename="../qml/pages/SourcesPage.qml" line="68"/>
         <source>Update in progress. This may take a while, but you can safely close the application and the update will finish in the background.</source>
-        <translation>Actualizacion en curso. Esto puede tardar un rato, pero puedes cerrar la aplicacion con seguridad y la actualizacion se completara en segundo plano.</translation>
+        <translation>Actualización en curso. Esto puede tardar un rato, pero puedes cerrar la aplicación con seguridad y la actualización se completará en segundo plano.</translation>
     </message>
 </context>
 <context>
@@ -325,17 +337,17 @@
     <message>
         <location filename="../qml/pages/WelcomePage.qml" line="27"/>
         <source>Adblock Lists</source>
-        <translation type="unfinished"></translation>
+        <translation>Listas bloqueo</translation>
     </message>
     <message>
         <location filename="../qml/pages/WelcomePage.qml" line="34"/>
         <source>Cookies</source>
-        <translation type="unfinished">Cookies</translation>
+        <translation>Cookies</translation>
     </message>
     <message>
         <location filename="../qml/pages/WelcomePage.qml" line="40"/>
         <source>Documentation</source>
-        <translation>Documentacion</translation>
+        <translation>Documentación</translation>
     </message>
     <message>
         <location filename="../qml/pages/WelcomePage.qml" line="46"/>
@@ -348,7 +360,7 @@
     <message>
         <location filename="../qml/harbour-defender.qml" line="13"/>
         <source>Defender</source>
-        <translation type="unfinished"></translation>
+        <translation>Defender</translation>
     </message>
 </context>
 </TS>
