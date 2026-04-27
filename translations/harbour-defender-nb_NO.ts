055f9d94cb7895ecc0245134ec23d42f148020b8
Frank Paul Silye <frankps@gmail.com>
2026-04-27 21:04:58 +0200

    nb_NO translations
    Complete translation to Norwegian bokmål (nb_NO.ts).

---
+++ translations/harbour-defender-nb_NO.ts
@@ -0,0 +1,350 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nb_NO">
+<context>
+    <name>CookiesMenuItem</name>
+    <message>
+        <location filename="../qml/pages/components/CookiesMenuItem.qml" line="23"/>
+        <source>Cookies</source>
+        <translation>Informasjonskapsler</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/components/CookiesMenuItem.qml" line="28"/>
+        <source>Domains</source>
+        <translation>Domener</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/components/CookiesMenuItem.qml" line="34"/>
+        <source>Cookie Manager</source>
+        <translation>Behandler for informasjonskapsler</translation>
+    </message>
+</context>
+<context>
+    <name>CookiesPage</name>
+    <message>
+        <location filename="../qml/pages/CookiesPage.qml" line="14"/>
+        <source>Search cookies</source>
+        <translation>Søk i informasjonskapsler</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CookiesPage.qml" line="29"/>
+        <source>Unlock cookies</source>
+        <translation>Lås opp informasjonskapsler</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CookiesPage.qml" line="29"/>
+        <source>Lock cookies</source>
+        <translation>Lås informasjonskapsler</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CookiesPage.qml" line="31"/>
+        <source>Unlocking</source>
+        <translation>Låser opp</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CookiesPage.qml" line="31"/>
+        <source>Locking</source>
+        <translation>Låser</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CookiesPage.qml" line="40"/>
+        <source>Delete all blacklisted</source>
+        <translation>Slett alle svartelistede</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CookiesPage.qml" line="42"/>
+        <location filename="../qml/pages/CookiesPage.qml" line="58"/>
+        <location filename="../qml/pages/CookiesPage.qml" line="83"/>
+        <location filename="../qml/pages/CookiesPage.qml" line="162"/>
+        <source>Deleting</source>
+        <translation>Sletter</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CookiesPage.qml" line="56"/>
+        <source>Delete all not whitelisted</source>
+        <translation>Slett alle som ikke er hvitelistet</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CookiesPage.qml" line="111"/>
+        <source>Remove</source>
+        <translation>Fjern</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CookiesPage.qml" line="115"/>
+        <source>Remove from Whitelist</source>
+        <translation>Fjern fra hviteliste</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CookiesPage.qml" line="115"/>
+        <source>Add to Whitelist</source>
+        <translation>Legg til i hviteliste</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CookiesPage.qml" line="134"/>
+        <source>Remove from Blacklist</source>
+        <translation>Fjern fra svarteliste</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CookiesPage.qml" line="134"/>
+        <source>Add to Blacklist</source>
+        <translation>Legg til i svarteliste</translation>
+    </message>
+</context>
+<context>
+    <name>DocsPage</name>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="19"/>
+        <source>Version</source>
+        <translation>Versjon</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="30"/>
+        <source>Warning</source>
+        <translation>Advarsel</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="35"/>
+        <source>The installation and use of this application is solely in the responsibility of the user. The developers/authors are not responsible for the content of ad blocking sources available from the application, therefore you need to be careful when enabling them.</source>
+        <translation>Installasjon og bruk av denne applikasjonen skjer utelukkende på brukerens eget ansvar. Utviklerne/forfatterne er ikke ansvarlige for innholdet i kildene for reklameblokkering som er tilgjengelige i appen, så vær forsiktig når du aktiverer dem.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="41"/>
+        <source>Adblock Lists</source>
+        <translation>Reklameblokkeringslister</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="44"/>
+        <source>How to add custom lists?</source>
+        <translation>Hvordan legge til egne lister?</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="49"/>
+        <source>You can add custom lists by editing the file /etc/defender.conf as root (either using the command line or an appropriate editor). See other sections in the config file for inspiration. In the square brackets [] should be a unique id.</source>
+        <translation>Du kan legge til egne lister ved å redigere filen /etc/defender.conf som root (enten via kommandolinjen eller et egnet redigeringsverktøy). Se andre seksjoner i konfigurasjonsfilen for inspirasjon. Mellom klammeparentesene [] må det stå en unik ID.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="55"/>
+        <source>Why can&apos;t I add new sources from the app?</source>
+        <translation>Hvorfor kan jeg ikke legge til nye kilder fra appen?</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="60"/>
+        <source>It would be a security threat to allow adding new sources as a normal user (with the current SailfishOS security situation), as any app would be able to add new sources and potentially compromise your device. Even with the default sources, you still make the leap of faith to trust a remote source. If you want to have a source added/removed to/from the defaults, contact the app developer and see if it can be available in the next version.</source>
+        <translation>Det ville vært en sikkerhetsrisiko å tillate tilføying av nye kilder som vanlig bruker (gitt dagens sikkerhetssituasjon i SailfishOS), da enhver app kunne lagt til kilder og potensielt kompromittert enheten din. Selv med standardkildene må du stole på en ekstern kilde. Hvis du ønsker at en kilde skal legges til eller fjernes fra standardvalgene, kontakt utvikleren for å se om den kan inkluderes i neste versjon.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="66"/>
+        <source>How to add custom entries?</source>
+        <translation>Hvordan legge til egne oppføringer?</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="71"/>
+        <source>You can add your custom hosts entries by editing the file /etc/hosts.editable and treating it as a generic hosts file. Don&apos;t forget to choose &apos;Update Now&apos; in the app to see an immediate effect.</source>
+        <translation>Du kan legge til egne verts-oppføringer (hosts) ved å redigere filen /etc/hosts.editable som en vanlig hosts-fil. Husk å velge «Oppdater nå» i appen for å se umiddelbar effekt.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="77"/>
+        <source>Why can&apos;t I add new entries from the app?</source>
+        <translation>Hvorfor kan jeg ikke legge til nye oppføringer fra appen?</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="82"/>
+        <source>Again, it would be a security threat (see the answer above), therefore one needs to be root to add/modify hosts file entries.</source>
+        <translation>Igjen, det ville utgjort en sikkerhetsrisiko (se svaret over), derfor må man være root for å legge til eller endre oppføringer i hosts-filen.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="88"/>
+        <source>Cookies</source>
+        <translation>Informasjonskapsler</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="93"/>
+        <source>The cookie manager works by editing the -/.local/share/org.sailfishos/browser/.mozilla/ (for SFOS up to 3.4: -/.mozilla/mozembed/) cookies.sqlite database. In order to access/work with cookies the browser needs to be closed before opening the cookies section, else you might see an empty window. All changes need the browser to be closed/restarted in order to take effect.</source>
+        <translation>Behandleren for informasjonskapsler fungerer ved å redigere databasen cookies.sqlite i ~/.local/share/org.sailfishos/browser/.mozilla/ (for SFOS opp til 3.4: ~/.mozilla/mozembed/). For å få tilgang til informasjonskapsler må nettleseren lukkes før du åpner denne seksjonen, ellers kan vinduet se tomt ut. Alle endringer krever at nettleseren lukkes eller startes på nytt for å tre i kraft.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="99"/>
+        <source>Cookie Locking</source>
+        <translation>Låsing av informasjonskapsler</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="104"/>
+        <source>Cookie locking works by making the cookie database read only, therefore its contents stay same between restarting the browser. The effect of this is cookies not being persistent over browser restarts.</source>
+        <translation>Låsing fungerer ved å skrivebeskytte databasen for informasjonskapsler, slik at innholdet forblir uendret mellom omstarter av nettleseren. Effekten er at informasjonskapsler ikke lagres permanent over økter.</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="19"/>
+        <source>Settings</source>
+        <translation>Innstillinger</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="22"/>
+        <source>Clear Cookie Blacklist</source>
+        <translation>Tøm svarteliste for informasjonskapsler</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="25"/>
+        <location filename="../qml/pages/SettingsPage.qml" line="36"/>
+        <source>Clearing</source>
+        <translation>Tømmer</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="33"/>
+        <source>Clear Cookie Whitelist</source>
+        <translation>Tøm hviteliste for informasjonskapsler</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="44"/>
+        <source>WLAN only</source>
+        <translation>Kun WLAN</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="45"/>
+        <source>Downloads adblock lists only if connected to WLAN (tested only on Jolla phones)</source>
+        <translation>Laster kun ned blokkeringslister når tilkoblet WLAN (kun testet på Jolla-telefoner)</translation>
+    </message>
+</context>
+<context>
+    <name>SourceDetailPage</name>
+    <message>
+        <location filename="../qml/pages/SourceDetailPage.qml" line="15"/>
+        <source>Open hosts file URL</source>
+        <translation>Åpne vertsfil-URL</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourceDetailPage.qml" line="19"/>
+        <source>More information</source>
+        <translation>Mer informasjon</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourceDetailPage.qml" line="43"/>
+        <source>Source</source>
+        <translation>Kilde</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourceDetailPage.qml" line="47"/>
+        <source>License</source>
+        <translation>Lisens</translation>
+    </message>
+</context>
+<context>
+    <name>SourcesMenuItem</name>
+    <message>
+        <location filename="../qml/pages/components/SourcesMenuItem.qml" line="23"/>
+        <source>Blocked</source>
+        <translation>Blokkert</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/components/SourcesMenuItem.qml" line="28"/>
+        <source>Lists</source>
+        <translation>Lister</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/components/SourcesMenuItem.qml" line="34"/>
+        <source>Adblock Lists</source>
+        <translation>Reklameblokkeringslister</translation>
+    </message>
+</context>
+<context>
+    <name>SourcesPage</name>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="13"/>
+        <source>Disable all</source>
+        <translation>Deaktiver alle</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="15"/>
+        <source>Preparing disable</source>
+        <translation>Forbereder deaktivering</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="21"/>
+        <source>Cancel/clear update loop</source>
+        <translation>Avbryt/tøm oppdateringsløkke</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="23"/>
+        <source>Preparing cancel/clear</source>
+        <translation>Forbereder avbryt/tøm</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="29"/>
+        <source>Show error log (just in case ;)</source>
+        <translation>Vis feillogg (bare i tilfelle ;)</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="31"/>
+        <source>Pulling up error.log (only if exists)</source>
+        <translation>Henter error.log (hvis den finnes)</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="37"/>
+        <source>Restart Android/App Support</source>
+        <translation>Start Android/App-støtte på nytt</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="39"/>
+        <source>Preparing Android restart</source>
+        <translation>Forbereder omstart av Android</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="45"/>
+        <source>Update now</source>
+        <translation>Oppdater nå</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="47"/>
+        <source>Preparing update</source>
+        <translation>Forbereder oppdatering</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="56"/>
+        <source>Sources</source>
+        <translation>Kilder</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="62"/>
+        <source>Updating...</source>
+        <translation>Oppdaterer...</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="68"/>
+        <source>Update in progress. This may take a while, but you can safely close the application and the update will finish in the background.</source>
+        <translation>Oppdatering pågår. Dette kan ta litt tid, men du kan trygt lukke programmet – oppdateringen vil fullføres i bakgrunnen.</translation>
+    </message>
+</context>
+<context>
+    <name>WelcomePage</name>
+    <message>
+        <location filename="../qml/pages/WelcomePage.qml" line="27"/>
+        <source>Adblock Lists</source>
+        <translation>Reklameblokkeringslister</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WelcomePage.qml" line="34"/>
+        <source>Cookies</source>
+        <translation>Informasjonskapsler</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WelcomePage.qml" line="40"/>
+        <source>Documentation</source>
+        <translation>Dokumentasjon</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WelcomePage.qml" line="46"/>
+        <source>Settings</source>
+        <translation>Innstillinger</translation>
+    </message>
+</context>
+<context>
+    <name>harbour-defender</name>
+    <message>
+        <location filename="../qml/harbour-defender.qml" line="13"/>
+        <source>Defender</source>
+        <translation>Defender</translation>
+    </message>
+</context>
+</TS>
