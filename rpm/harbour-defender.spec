9167a628d20faa5e01ba48a423af7f8b129ea99d
Peter Leinchen <peterleinchen@t-online.de>
2026-08-23 11:24:47 +0200

    v0.9.7 - added Nederlands and Vlaam langugage ts (courtesy of @nthn)

--- rpm/harbour-defender.spec
+++ rpm/harbour-defender.spec
@@ -15,7 +15,7 @@
 
 Name:       harbour-defender
 Summary:    A privacy guard for SFOS
-Version:    0.9.6
+Version:    0.9.7
 Release:    1
 Group:      Qt/Qt
 License:    GPLv3
---
+++ translations/harbour-defender-nl.ts
@@ -0,0 +1,350 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl">
+<context>
+    <name>CookiesMenuItem</name>
+    <message>
+        <location filename="../qml/pages/components/CookiesMenuItem.qml" line="23"></location>
+        <source>Cookies</source>
+        <translation>Cookies</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/components/CookiesMenuItem.qml" line="28"></location>
+        <source>Domains</source>
+        <translation>Domeinen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/components/CookiesMenuItem.qml" line="34"></location>
+        <source>Cookie Manager</source>
+        <translation>Cookiebeheerder</translation>
+    </message>
+</context>
+<context>
+    <name>CookiesPage</name>
+    <message>
+        <location filename="../qml/pages/CookiesPage.qml" line="14"></location>
+        <source>Search cookies</source>
+        <translation>Cookies doorzoeken</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CookiesPage.qml" line="29"></location>
+        <source>Unlock cookies</source>
+        <translation>Cookies ontgrendelen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CookiesPage.qml" line="29"></location>
+        <source>Lock cookies</source>
+        <translation>Cookies vergrendelen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CookiesPage.qml" line="31"></location>
+        <source>Unlocking</source>
+        <translation>Cookies worden ontgrendeld</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CookiesPage.qml" line="31"></location>
+        <source>Locking</source>
+        <translation>Cookies worden vergrendeld</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CookiesPage.qml" line="40"></location>
+        <source>Delete all blacklisted</source>
+        <translation>Alles op de zwarte lijst verwijderen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CookiesPage.qml" line="42"></location>
+        <location filename="../qml/pages/CookiesPage.qml" line="58"></location>
+        <location filename="../qml/pages/CookiesPage.qml" line="83"></location>
+        <location filename="../qml/pages/CookiesPage.qml" line="162"></location>
+        <source>Deleting</source>
+        <translation>Wordt verwijderd</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CookiesPage.qml" line="56"></location>
+        <source>Delete all not whitelisted</source>
+        <translation>Alles niet op de witte lijst verwijderen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CookiesPage.qml" line="111"></location>
+        <source>Remove</source>
+        <translation>Verwijderen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CookiesPage.qml" line="115"></location>
+        <source>Remove from Whitelist</source>
+        <translation>Verwijderen van witte lijst</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CookiesPage.qml" line="115"></location>
+        <source>Add to Whitelist</source>
+        <translation>Toevoegen aan witte lijst</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CookiesPage.qml" line="134"></location>
+        <source>Remove from Blacklist</source>
+        <translation>Verwijderen van zwarte lijst</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CookiesPage.qml" line="134"></location>
+        <source>Add to Blacklist</source>
+        <translation>Toevoegen aan zwarte lijst</translation>
+    </message>
+</context>
+<context>
+    <name>DocsPage</name>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="19"></location>
+        <source>Version</source>
+        <translation>Versie</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="30"></location>
+        <source>Warning</source>
+        <translation>Waarschuwing</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="35"></location>
+        <source>The installation and use of this application is solely in the responsibility of the user. The developers/authors are not responsible for the content of ad blocking sources available from the application, therefore you need to be careful when enabling them.</source>
+        <translation>De installatie en het gebruik van deze applicatie zijn geheel de verantwoordelijkheid van de gebruiker. De ontwikkelaars/auteurs zijn niet verantwoordelijk voor de inhoud van de reclameblokkeringsbronnen die de applicatie gebruikt, dus wees voorzichtig wanneer je ze inschakelt.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="41"></location>
+        <source>Adblock Lists</source>
+        <translation>Reclameblokkeringslijsten</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="44"></location>
+        <source>How to add custom lists?</source>
+        <translation>Hoe voeg ik aangepaste lijsten toe?</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="49"></location>
+        <source>You can add custom lists by editing the file /etc/defender.conf as root (either using the command line or an appropriate editor). See other sections in the config file for inspiration. In the square brackets [] should be a unique id.</source>
+        <translation>Je kunt aangepaste lijsten toevoegen door het bestand /etc/defender.conf te bewerken als root (via de opdrachtregel of met een geschikte tekstbewerker). Bekijk de overige onderdelen van het configuratiebestand voor inspiratie. Tussen de rechte haakjes moet een unieke ID staan.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="55"></location>
+        <source>Why can't I add new sources from the app?</source>
+        <translation>Waarom kan ik niet rechtstreeks in de app nieuwe bronnen toevoegen?</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="60"></location>
+        <source>It would be a security threat to allow adding new sources as a normal user (with the current SailfishOS security situation), as any app would be able to add new sources and potentially compromise your device. Even with the default sources, you still make the leap of faith to trust a remote source. If you want to have a source added/removed to/from the defaults, contact the app developer and see if it can be available in the next version.</source>
+        <translation>Het zou een beveiligingsrisico vormen als je (binnen de huidige beveiliging van Sailfish OS) nieuwe bronnen zou kunnen toevoegen als normale gebruiker, omdat om het even welke app dan nieuwe bronnen zou kunnen toevoegen, en potentieel je apparaat compromitteren. Zelfs met de standaardbronnen vertrouw je nog altijd een bron van een derde partij. Als je een bron aan de standaardbronnen toegevoegd wilt zien, of net eruit verwijderd, neem dan contact op met de ontwikkelaar en kijk of dat misschien tegen de volgende versie zou kunnen.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="66"></location>
+        <source>How to add custom entries?</source>
+        <translation>Hoe voeg ik aangepaste ingangen toe?</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="71"></location>
+        <source>You can add your custom hosts entries by editing the file /etc/hosts.editable and treating it as a generic hosts file. Don't forget to choose 'Update Now' in the app to see an immediate effect.</source>
+        <translation>Je kunt je aangepaste hosts-ingangen toevoegen door het bestand /etc/hosts.editable te bewerken, en het te behandelen als een generisch hosts-bestand. Vergeet niet in de app ‘Nu bijwerken’ te gebruiken voor een onmiddellijk effect.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="77"></location>
+        <source>Why can't I add new entries from the app?</source>
+        <translation>Waarom kan ik niet rechtstreeks in de app nieuwe ingangen toevoegen?</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="82"></location>
+        <source>Again, it would be a security threat (see the answer above), therefore one needs to be root to add/modify hosts file entries.</source>
+        <translation>Het zou ook hier een beveiligingsrisico vormen (zie het antwoord hierboven), daarom moet je root zijn om hosts-bestandsingangen toe te voegen of te wijzigen.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="88"></location>
+        <source>Cookies</source>
+        <translation>Cookies</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="93"></location>
+        <source>The cookie manager works by editing the -/.local/share/org.sailfishos/browser/.mozilla/ (for SFOS up to 3.4: -/.mozilla/mozembed/) cookies.sqlite database. In order to access/work with cookies the browser needs to be closed before opening the cookies section, else you might see an empty window. All changes need the browser to be closed/restarted in order to take effect.</source>
+        <translation>De cookiebeheerder werkt door de cookies.sqlite-database in -/.local/share/org.sailfishos/browser/.mozilla/ (voor SFOS tot 3.4: -/.mozilla/mozembed/) te bewerken. Om cookies te kunnen gebruiken moet de browser eerst gesloten worden voordat je het cookies-onderdeel opent, anders krijg je misschien een leeg venster te zien. Je moet de browser sluiten en opnieuw starten om de wijzigingen door te voeren.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="99"></location>
+        <source>Cookie Locking</source>
+        <translation>Cookies vergrendelen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DocsPage.qml" line="104"></location>
+        <source>Cookie locking works by making the cookie database read only, therefore its contents stay same between restarting the browser. The effect of this is cookies not being persistent over browser restarts.</source>
+        <translation>Cookies vergrendelen werkt door de cookiedatabase op alleen-lezen in te stellen, waardoor de inhoud hetzelfde blijft bij elke nieuwe start van de browser. Dit heeft als gevolg dat cookies niet blijven bestaan tussen browserherstarts.</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="19"></location>
+        <source>Settings</source>
+        <translation>Instellingen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="22"></location>
+        <source>Clear Cookie Blacklist</source>
+        <translation>Zwarte lijst van cookies wissen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="25"></location>
+        <location filename="../qml/pages/SettingsPage.qml" line="36"></location>
+        <source>Clearing</source>
+        <translation>Wordt gewist</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="33"></location>
+        <source>Clear Cookie Whitelist</source>
+        <translation>Witte lijst van cookies wissen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="44"></location>
+        <source>WLAN only</source>
+        <translation>Enkel WLAN</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="45"></location>
+        <source>Downloads adblock lists only if connected to WLAN (tested only on Jolla phones)</source>
+        <translation>Downloadt reclameblokkeringslijsten enkel indien verbonden met WLAN (enkel getest op Jolla-telefoons)</translation>
+    </message>
+</context>
+<context>
+    <name>SourceDetailPage</name>
+    <message>
+        <location filename="../qml/pages/SourceDetailPage.qml" line="15"></location>
+        <source>Open hosts file URL</source>
+        <translation>Hosts-bestands-URL openen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourceDetailPage.qml" line="19"></location>
+        <source>More information</source>
+        <translation>Meer informatie</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourceDetailPage.qml" line="43"></location>
+        <source>Source</source>
+        <translation>Bron</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourceDetailPage.qml" line="47"></location>
+        <source>License</source>
+        <translation>Licentie</translation>
+    </message>
+</context>
+<context>
+    <name>SourcesMenuItem</name>
+    <message>
+        <location filename="../qml/pages/components/SourcesMenuItem.qml" line="23"></location>
+        <source>Blocked</source>
+        <translation>Geblokkeerd</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/components/SourcesMenuItem.qml" line="28"></location>
+        <source>Lists</source>
+        <translation>Lijsten</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/components/SourcesMenuItem.qml" line="34"></location>
+        <source>Adblock Lists</source>
+        <translation>Reclameblokkeringslijsten</translation>
+    </message>
+</context>
+<context>
+    <name>SourcesPage</name>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="13"></location>
+        <source>Disable all</source>
+        <translation>Alles uitschakelen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="15"></location>
+        <source>Preparing disable</source>
+        <translation>Uitschakelen wordt voorbereid</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="21"></location>
+        <source>Cancel/clear update loop</source>
+        <translation>Updatelus annuleren/wissen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="23"></location>
+        <source>Preparing cancel/clear</source>
+        <translation>Annulatie/wissen wordt voorbereid</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="29"></location>
+        <source>Show error log (just in case ;)</source>
+        <translation>Foutlog tonen (voor het geval dat ;))</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="31"></location>
+        <source>Pulling up error.log (only if exists)</source>
+        <translation>Foutlog wordt opgehaald (enkel als het bestaat)</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="37"></location>
+        <source>Restart Android/App Support</source>
+        <translation>Android/App Support herstarten</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="39"></location>
+        <source>Preparing Android restart</source>
+        <translation>Herstart van Android wordt voorbereid</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="45"></location>
+        <source>Update now</source>
+        <translation>Nu bijwerken</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="47"></location>
+        <source>Preparing update</source>
+        <translation>Update wordt voorbereid</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="56"></location>
+        <source>Sources</source>
+        <translation>Bronnen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="62"></location>
+        <source>Updating...</source>
+        <translation>Update wordt uitgevoerd...</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SourcesPage.qml" line="68"></location>
+        <source>Update in progress. This may take a while, but you can safely close the application and the update will finish in the background.</source>
+        <translation>De update wordt uitgevoerd. Dit kan eventjes duren, maar je kunt de applicatie veilig sluiten. De update zal op de achtergrond uitgevoerd worden.</translation>
+    </message>
+</context>
+<context>
+    <name>WelcomePage</name>
+    <message>
+        <location filename="../qml/pages/WelcomePage.qml" line="27"></location>
+        <source>Adblock Lists</source>
+        <translation>Reclameblokkeringslijsten</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WelcomePage.qml" line="34"></location>
+        <source>Cookies</source>
+        <translation>Cookies</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WelcomePage.qml" line="40"></location>
+        <source>Documentation</source>
+        <translation>Documentatie</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WelcomePage.qml" line="46"></location>
+        <source>Settings</source>
+        <translation>Instellingen</translation>
+    </message>
+</context>
+<context>
+    <name>harbour-defender</name>
+    <message>
+        <location filename="../qml/harbour-defender.qml" line="13"></location>
+        <source>Defender</source>
+        <translation>Beschermer</translation>
+    </message>
+</context>
+</TS>
